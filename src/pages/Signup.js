e7f422168bf2b7d12e71426b42fef07d2726d876
coolBrown12 <yash91238@gmail.com>
2023-07-07 00:14:38 +0530

    with frontend

---
+++ src/pages/Signup.js
@@ -0,0 +1,10 @@
+import Signuppages from "../features/auth/components/Signup";
+
+function Signup(){
+    return(
+        <div>
+            <Signuppages></Signuppages>
+        </div>
+    )
+}
+export default Signup;
